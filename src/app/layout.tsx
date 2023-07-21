7bd6440da98ee01732b9dba1b46158f83ba59dbe
ArtemSmirnow <Smirnow_999@mail.ru>
2023-07-21 20:33:19 +0300

    add global background #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style = {{
+      backgroundImage: 'url("/media/images/background.png")',
+      backgroundSize:"cover"
+      }}>
         {children}
       </body>
     </html>

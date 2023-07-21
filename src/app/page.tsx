ede27b5af1642cd4d60f21ac42043c45e1e3fabf
ArtemSmirnow <Smirnow_999@mail.ru>
2023-07-21 20:42:45 +0300

    add header and welcome block components to main page #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Header from './components/Header'
+import Header from './components/header'
+import WelcomeBlock from './components/main/WelcomeBlock'
 
 export default function Home() {
   return (
     <main>
       <Header/>
+      <WelcomeBlock/>
     </main>
   )
 }

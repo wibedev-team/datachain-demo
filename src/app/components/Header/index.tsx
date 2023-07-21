e7ee450869e92a2c0f4e8220be53ca57f998ce54
ArtemSmirnow <Smirnow_999@mail.ru>
2023-07-21 20:38:13 +0300

    add burger component and planet image in header #3

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,9 +1,15 @@
-import NavigationOptions from "./NavigationOptions";
+import PlanetImage from "./navigtionBar/PlanetImage";
+import NavigationOptions from "./navigtionBar/listNavigation/NavigationOptions";
+import BurgerNavigation from "./navigtionBar/burgerNavigation/BurgerNavigation";
 
 const Header = () => {
-    return ( 
-        <div className="w-full pt-20 pl-32  items-center">
+    return (
+        <div className="w-full items-center
+        md:pt-20 md:pl-16
+        lg:pl-16">
+            <BurgerNavigation/>
             <NavigationOptions/>
+            <PlanetImage/>
         </div>
         
         

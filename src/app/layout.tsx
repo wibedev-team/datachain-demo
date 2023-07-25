21e1b168137a56b142ce9c8d7394198aad3f71a2
ArtemSmirnow <Smirnow_999@mail.ru>
2023-07-25 21:16:18 +0300

    add icon in tytle #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/Logo.ico" type="image/x-icon"/>
+      </head>
       <body className={inter.className} style = {{
       backgroundImage: 'url("/media/images/background.png")',
-      backgroundSize:"cover"
+      backgroundSize:"cover",
+      backgroundRepeat: 'no-repeat'
       }}>
         {children}
       </body>

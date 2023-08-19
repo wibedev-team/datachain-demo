44f962a0e6dc5d670c736c99481733f7b0fc870f
ArtemSmirnow <Smirnow_999@mail.ru>
2023-08-19 19:19:04 +0300

    deleted files after changing the file structure #3

--- src/app/components/main/WelcomeBlock/WriteToUsIcon.tsx
+++
@@ -1,9 +0,0 @@
-const WriteToUsIcon = () => {
-    return ( <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 18.7508L15.905 9.49989L0 0.249023V7.44413L11.3935 9.49989L0 11.5557V18.7508Z" fill="white"/>
-    </svg>
-    
-     );
-}
- 
-export default WriteToUsIcon;

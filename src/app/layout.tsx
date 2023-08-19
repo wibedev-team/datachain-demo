19f696b7610b609f75d58845f03d44a3adbc73b6
ArtemSmirnow <Smirnow_999@mail.ru>
2023-08-19 19:25:39 +0300

    add background style #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'DataCHAIN',
-  description: 'Essential software tools for analytics, visualization, automatization and monitoring.',
-}
+  title: "DataCHAIN",
+  description:
+    "Essential software tools for analytics, visualization, automatization and monitoring.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/Logo.ico" type="image/x-icon"/>
+        <link rel="icon" href="/Logo.ico" type="image/x-icon" />
       </head>
-      <body className={inter.className} style = {{
-      backgroundImage: 'url("/media/images/background.png")',
-      backgroundSize:"cover",
-      backgroundRepeat: 'no-repeat'
-      }}>
-        {children}
-      </body>
+      <body className={`${inter.className} background`}>{children}</body>
     </html>
-  )
+  );
 }

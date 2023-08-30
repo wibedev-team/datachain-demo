1ce8a0e6c3b3bf114f522a1b2620ee0215466160
AlexanderHanziuk <aleksandrtiskov488@gmail.com>
2023-08-30 19:34:56 +0300

    add desktop water section #13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import {WelcomeBlock} from "@widgets/WelcomeBlock/WelcomeBlock";
-import {Header} from "@widgets/Header/Header";
+import React from 'react'
+import { WelcomeBlock } from '@widgets/WelcomeBlock/WelcomeBlock'
+import { Header } from '@widgets/Header/Header'
+import { WaterSection } from '@widgets/WaterSection/WaterSection'
 
 export default function Home() {
   return (
     <main>
       <Header />
       <WelcomeBlock />
+      <WaterSection />
     </main>
-  );
+  )
 }

924fe10223e942c041665138355516c8dd19b03a
Ilya <gurinovich-ilya@list.ru>
2023-08-30 15:04:22 +0300

    fix: styles

--- src/widgets/IndustryBlock/IndustryBlock.tsx
+++ src/widgets/IndustryBlock/IndustryBlock.tsx
@@ -19,22 +19,29 @@ export const IndustryBlock = () => {
       <div
         className="flex flex-col items-start
         mt-[20px]
-        sm:mt-[25px] sm:flex-row-reverse
-        md:mt-[38px] md:flex-row-reverse
-        xl:mt-[60px] xl:flex-row-reverse">
+        sm:mt-[25px]
+        md:mt-[38px]
+        xl:mt-[60px]">
         <div
-          className="w-[335px] h-[157px] shrink-0  mt-[60px] mb-[60px]
+          className="flex flex-col h-[345px] 
+        sm:h-[38px] sm:flex-row 
+        md:h-[58px] md:flex-row
+        xl:h-[90px] xl:flex-row">
+          <SectionSubtitle text={'TV & broadcast virtual production'} className="xl:mb-[60px]" />
+          <div
+            className="w-[335px] h-[157px] shrink-0  mt-[60px] mb-[60px]
                 sm:w-[340px] sm:h-[180px] sm:ml-[30px] 
                 md:w-[530px] md:h-[306px] md:ml-[70px]
                 xl:w-[819px] xl:h-[429px] xl:ml-[60px]">
-          <iframe width="100%" height="100%" src="https://www.youtube.com/embed/AVvcaNerxok" allowFullScreen></iframe>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/AVvcaNerxok" allowFullScreen></iframe>
+          </div>
         </div>
+
         <div
           className="shrink-0 
             sm:mb-[25px] sm:pl-[60px]
             md:mb-[38px] md:pl-[50px]
             xl:pl-[138px]">
-          <SectionSubtitle text={'TV & broadcast virtual production'} className="mb-[60px] xl:mb-[60px]" />
           <SectionList list={DataList} title={'DATA'} className={'mb-[1.875rem]'} />
           <SectionList list={ToolsList} title={'TOOLS'} className={'mb-[30px] sm:mb-[25px] md:mb-[53px] xl:mb-[60px]'} />
           <SectionButton text={'REQUEST A SOLUTION'} />

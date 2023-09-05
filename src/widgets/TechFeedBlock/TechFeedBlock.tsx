c0fe07ce1fe5319ecfcd85bc7afd137b52ebbfd9
Alexandr Zaycev <hunter290592@yandex.ru>
2023-09-05 19:54:37 +0300

    add adaptive

--- src/widgets/TechFeedBlock/TechFeedBlock.tsx
+++ src/widgets/TechFeedBlock/TechFeedBlock.tsx
@@ -4,10 +4,23 @@ import Image from 'next/image'
 
 export const TechFeedBlock = () => {
   return (
-    <section className="my-[150px] px-[20px] h-[50px]">
-      <ul className="flex flex-row items-center gap-[70px] overflow-x-auto no-scrollbar">
+    <section
+      className="
+	 vs:h-[13px]
+	 sm:mt-[65px] sm:h-[21px] mt-[60px]
+	 md:mt-[100px] md:h-[32px]
+	 xl:my-[150px] xl:px-[20px] xl:h-[50px]
+	 ">
+      <ul
+        className="
+		flex flex-row items-center overflow-x-auto no-scrollbar h-full flex-shrink-0
+		vs:gap-[18px]
+		sm:gap-[29px]
+		md:gap-[45px]
+		xl:gap-[70px]
+		">
         {icons.map((icon, index) => (
-          <Image key={`iconKey${index}`} src={icon} alt={'icon'} />
+          <Image key={`iconKey${index}`} src={icon} alt={`Icon${index}`} className="h-full object-contain" />
         ))}
       </ul>
     </section>

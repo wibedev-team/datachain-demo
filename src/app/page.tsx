2e294d099b952cb2bc7561b8d46ef34118aee33c
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2023-09-07 15:55:39 +0500

    added footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {WelcomeBlock} from "@widgets/WelcomeBlock/WelcomeBlock";
 import {Header} from "@widgets/Header/Header";
 import {AboutUsBlock} from "@widgets/AboutUsBlock/AboutUs" 
+import {Footer} from "@widgets/Footer/Footer"
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <WelcomeBlock />
       <AboutUsBlock/>
+      <Footer/>
     </main>
   );
 }
